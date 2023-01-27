2aeaca694939e88aa78e72363db32c6255eef104
unknown <guilhermekhirata@hotmail.com>
2023-01-27 13:23:06 -0300

    feat: criação das configurações do postgres

--- _project/settings.py
+++ _project/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = "_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB_NAME"),
+        "USER": os.getenv("POSTGRES_USERNAME"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
